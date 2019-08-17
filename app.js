9420d98f8e43705be7703a6464b7e08635664f24
harshendrashah <harshendrashah812@gmail.com>
2019-08-17 17:12:57 +0530

    Completed Reading a note functionality

--- app.js
+++ app.js
@@ -51,8 +51,15 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe: 'Read a specific note!',
-    handler () {
-        console.log('Reading a note!')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type:'string' 
+        }
+    },
+    handler (argv) {
+        notesUtilities.readNote(argv.title)
     }
 })
 
--- notes.js
+++ notes.js
@@ -7,12 +7,10 @@ const getNotes = () => {
 
 const addNote = (title, body) => {
     const notes = loadNotes()
-    
-    const duplicateNotes = notes.filter(
-        (note) => note.title === title
-    )
 
-    if (duplicateNotes.length === 0) {
+    const duplicateNote = notes.find((note) => note.title === title)
+
+    if (!duplicateNote) {
         notes.push({
             title: title,
             body: body
@@ -48,7 +46,20 @@ const listNotes = () => {
         });
     }
 }
- 
+
+const readNote = (title) => {
+    const notes = loadNotes()
+    
+    const queriedNote = notes.find((note) => note.title === title)
+
+    if(queriedNote) {
+        console.log(chalk.blue.inverse(queriedNote.title))
+        console.log(queriedNote.body)
+    } else {
+        console.log(chalk.red.inverse("Sorry, couldn't find the note!"))
+    }
+}
+
 const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
@@ -68,5 +79,6 @@ module.exports = {
     getNotes: getNotes,
     addNote: addNote,
     removeNote: removeNote,
-    listNotes: listNotes
+    listNotes: listNotes,
+    readNote: readNote
 }
